120b64ee20329289b0441b7bd2f0c30159d995f9
Amanda Ferreira <amandaferreiradasilvan@gmail.com>
2021-12-19 21:43:57 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Primeiro 
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = "Hello ";
+            var b = "World!";
+            Console.WriteLine(a + b);
+        }
+    }
+}
+
